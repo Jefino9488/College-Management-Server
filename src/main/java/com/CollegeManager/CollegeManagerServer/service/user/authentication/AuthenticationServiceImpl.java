00cbd151fdf9ff6e18a14886cb8a6b8340f12df2
Jefino9488 <jefinojacob9488@gmail.com>
2025-09-12 23:11:56 +0530

    optimize imports

--- src/main/java/com/CollegeManager/CollegeManagerServer/service/user/authentication/AuthenticationServiceImpl.java
+++ src/main/java/com/CollegeManager/CollegeManagerServer/service/user/authentication/AuthenticationServiceImpl.java
@@ -12,11 +12,7 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
