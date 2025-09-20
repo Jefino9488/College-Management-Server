0d29da2211e756f0d8570a6364dcdc8b47a11486
Jefino9488 <jefinojacob9488@gmail.com>
2025-09-20 13:25:53 +0530

    Allow Vercel deployment to access API via CORS.

--- src/main/java/com/CollegeManager/CollegeManagerServer/security/config/SecurityConfig.java
+++ src/main/java/com/CollegeManager/CollegeManagerServer/security/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://college-management-ad0jaej00-jefino9488s-projects.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
